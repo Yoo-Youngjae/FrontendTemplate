6d7b7b86223b6e8719f6ea6c852a733f1e71707f
YooYoungJae <dudwo235@snu.ac.kr>
2019-11-11 20:11:45 +0900

    backend second commit

--- backend/first/views.py
+++ backend/first/views.py
@@ -1,16 +1,54 @@
 import json
 
-
 from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 
 def first(request):
     return HttpResponse('Hello, World!')
-    # if request.method == 'GET':
-    #     req_data = json.loads(request.body.decode())
 
-        # newfever = Fever_history(category=req_data['category'],
-        #                          user=request.user,
-        #                          goalTime=req_data['goalTime'],
-        #                          etcCategory=req_data['etcCategory'])
+def signup(request):
+    if request.method == 'POST':
+        req_data = json.loads(request.body.decode())
+        try:
+            username = req_data['username']
+            password = req_data['password']
+        except KeyError:
+            return HttpResponse(status=400)
+        try:
+            newUser = User.objects.create_user(username=username, password=password)
+            newUser.save()
+            return HttpResponse(status=201)
+        except: # 실패시 401에러
+            return HttpResponse(status=401)
+    else:
+        return HttpResponse(status=405)
 
-        # return HttpResponse('Hello, World!')
+def signin(request):
+    if request.method == 'POST':
+        req_data = json.loads(request.body.decode())
+        try:
+            username = req_data['username']
+            password = req_data['password']
+        except KeyError:
+            return HttpResponse(status=400)
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse(status=204)
+        else:
+            return HttpResponse(status=401)
+    else:
+        return HttpResponse(status=405)
+
+def signout(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated: # login 된 상태라
+            logout(request)
+            return HttpResponse(status=204)
+        else:
+            return HttpResponse(status=401)
+    else:
+        return HttpResponse(status=405)
+
